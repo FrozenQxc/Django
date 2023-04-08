aa1d1a1f316dd5235de3e611932581d6b64ea8e5
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-04-08 17:01:28 +0300

    Delete personal_portfolio directory

--- personal_portfolio/portfolio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-# Ипортирование проекта 
-from .models import Project
-
-def home(request):
-    # получение данных из базы данных 
-    projects =  Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects':projects})
